168ec1fc8d2940ee8c9cecb067516f7d01815411
teamoteh <lim1473@gmail.com>
2021-10-07 03:02:44 +0800

    Updated code to include Description data

--- RestaurantReserveSystem/MainCourse.java
+++ RestaurantReserveSystem/MainCourse.java
@@ -3,10 +3,12 @@ package RestaurantReserveSystem;
 public class MainCourse {
     private String courseName;
     private double coursePrice;
+    private String courseDesc;
 
     public MainCourse(){
         courseName = null;
         coursePrice = 0.0;
+        courseDesc = null;
     }
 
     public MainCourse(String name, double price){
@@ -20,5 +22,21 @@ public class MainCourse {
 
     public double getCoursePrice(){
         return this.coursePrice;
+    }
+
+    public String getCourseDesc(){
+        return this.courseDesc;
+    }
+
+    public void setCourseName(String name){
+        this.courseName = name;
+    }
+
+    public void setCoursePrice(double price){
+        this.coursePrice = price;
+    }
+
+    public void setCourseDesc(String desc){
+        this.courseDesc = desc;
     }
 }
