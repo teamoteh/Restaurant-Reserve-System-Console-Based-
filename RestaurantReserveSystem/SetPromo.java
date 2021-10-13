19d1e19e733eddc5758d78c522e2fccaa0f4477f
teamoteh <lim1473@gmail.com>
2021-10-14 00:12:19 +0800

    Updated SetPromo getPromo()

--- RestaurantReserveSystem/SetPromo.java
+++ RestaurantReserveSystem/SetPromo.java
@@ -14,6 +14,7 @@ public class SetPromo {
         dessert = new Dessert();
         drink = new Drink();
         main = new MainCourse();
+        totalCost = 0;
     }
 
     public void updatePromoAppet(String appetName, double appetPrice){
@@ -32,7 +33,7 @@ public class SetPromo {
         this.main = new MainCourse(mainName, mainPrice);
     }
 
-    private double totalCost(Appetizer appet, Dessert dessert, MainCourse main, Drink drink){
+    protected double totalCost(Appetizer appet, Dessert dessert, MainCourse main, Drink drink){
         double total = 0;
         total += appet.getAppetPrice();
         total += dessert.getDessertPrice();
@@ -40,38 +41,24 @@ public class SetPromo {
         total += drink.getDrinkPrice();
 
         total *= DISC;
+        this.totalCost = total;
 
         return total;
     }
 
     public void getPromo(){
+        System.out.printf("The set promo for today is:\n");
         if(appet.getAppetName() != null){
-            if(main.getCourseName() != null){
-                if(dessert.getDessertName() != null){
-                    if(drink.getDrinkName() != null){
-                        System.out.printf("The set promo for today is:\n");
-                        System.out.printf("Appetizer: %s, %s\n", appet.getAppetName(), appet.getAppetDesc());
-                        System.out.printf("Course: %s, %s\n", main.getCourseName(), main.getCourseDesc());
-                        System.out.printf("Dessert: %s, %s\n", dessert.getDessertName(), dessert.getDessertDesc());
-                        System.out.printf("Drinks to go: %s, %s\n", drink.getDrinkName(), drink.getDrinkDesc());
-                    }
-                    else{
-                        System.out.printf("The set promo for today is:\n");
-                        System.out.printf("Appetizer: %s, %s\n", appet.getAppetName(), appet.getAppetDesc());
-                        System.out.printf("Course: %s, %s \n", main.getCourseName(), main.getCourseDesc());
-                        System.out.printf("Dessert: %s, %s \n", dessert.getDessertName(), dessert.getDessertDesc());
-                    }
-                }
-                else{
-                    System.out.printf("The set promo for today is:\n");
-                        System.out.printf("Appetizer: %s, %s\n", appet.getAppetName(), appet.getAppetDesc());
-                        System.out.printf("Course: %s, %s\n", main.getCourseName(), main.getCourseDesc());
-                }
-            }
-            else{
-                System.out.printf("The set promo for today is:");
-                        System.out.printf("Appetizer: %s, %s\n", appet.getAppetName(), appet.getAppetDesc());
-            }
+            System.out.printf("Appetizer: %s, %s\n", appet.getAppetName(), appet.getAppetDesc());
         }
+        if(main.getCourseName() != null){
+            System.out.printf("Course: %s, %s\n", main.getCourseName(), main.getCourseDesc());
+        }
+        if(dessert.getDessertName() != null){
+            System.out.printf("Dessert: %s, %s\n", dessert.getDessertName(), dessert.getDessertDesc());
+        }
+        if(drink.getDrinkName() != null){
+            System.out.printf("Drinks to go: %s, %s\n", drink.getDrinkName(), drink.getDrinkDesc());
+        }        
     }
 }
