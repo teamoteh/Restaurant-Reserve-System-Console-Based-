536cf679711823f9e3c9d4786878beb99eb413b8
KaiSheng Lim <[limkaisheng.work@gmail.com]>
2021-10-22 21:49:56 +0800

    Basic Structure

--- RestaurantReserveSystem/RestaurantApp.java
+++ RestaurantReserveSystem/RestaurantApp.java
@@ -1,1 +1,8 @@
+public class RestaurantApp{
 
+    public String currentTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+}
