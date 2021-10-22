6c6abedc112190be37190e494d00e4b09d3d7bae
KaiSheng Lim <[limkaisheng.work@gmail.com]>
2021-10-22 21:50:51 +0800

    Need to update Discount and FoodItem Quantity

--- RestaurantReserveSystem/OrderInvoice.java
+++ RestaurantReserveSystem/OrderInvoice.java
@@ -1,89 +1,87 @@
 package RestaurantReserveSystem;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class OrderInvoice {
-    private List<String> orderItems;
-    private String staffName;
-    private int tableNo;
+    // Pre-checkout: Used to track user order
+    // Post-checkout: Used to print the receipt
+    private boolean checkOut;
+
+    // TimeStamp - Real-time
     private String timeStamp;
-    private String dateStamp;
+
+    // User orders
+    private List<FoodItem> orderItems;
+    private int tableNo;
+
+    // Payment
     private double salesTax;
     private double serviceCharge;
     private double totalSum;
-    // added staffgender, employee id and job title
+
+    // Staff Object
+    private String staffName;
     private String staffGender;
-    private int staffID;
     private String jobTitle;
+    private int staffID;
 
+    public OrderInvoice(int tableNum, Staff staff) {
+        this.checkOut = false;
+        this.timeStamp = getTimeStamp();
 
-    public OrderInvoice(int tableNum, String timeStamp, String dateStamp, String staffName) {
-        this.orderItems = new ArrayList<String>();
-        this.tableNo = tableNum; // Added these 3 because I think it would be neccesary when creating a new
-                                 // invoice
+        this.orderItems = new ArrayList<FoodItem>();
+        this.tableNo = tableNum;
+
+        // Payment
+        this.salesTax = 0.10;
+        this.serviceTax = 0.10;
+        this.totalSum = 0;
+
+        // Staff info (gender, employee, id and job title) requested by prof
+        this.staffName = staff.getStaffName();
+        this.staffGender = staff.getStaffGender();
+        this.jobTitle = staff.getJobTitle();
+        this.staffID = staff.getStaffID();
+    };
+
+    public boolean getCheckOut() {
+        return this.checkOut;
+    }
+    
+    public void setCheckOut(boolean checkOut) {
+        this.checkOut = checkOut;
+        this.timeStamp = getTimeStamp();
+    }
+    
+    public String getTimeStamp() {
+        return this.timeStamp;
+    }
+    
+    public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
-        this.dateStamp = dateStamp;
-        this.staffName = staffName;
-
-        // as prof replied in the email that needed to add staff info (gender,employee id and job title)
-        this.staffGender = staffGender;
-        this.staffID = staffID;
-        this.jobTitle = jobTitle;
-
-
-    };
-
-    public List<String> getOrderItems() {
-        return this.orderItems;
-    };
+    }
 
     // Add items
-    public void addOrderItems(String OrderItems) {
+    public void addOrderItems(FoodItem OrderItems) {
         this.orderItems.add(OrderItems);
     };
+
     // Remove items
-    public void removeOrderItems(String OrderItems) {
+    public void removeOrderItems(FoodItem OrderItems) {
         this.orderItems.remove(OrderItems);
     };
 
-    public String getStaffName() {
-        return this.staffName;
+    public void printOrderItems() {
+        for (int i = 0; i < this.orderItems.size(); i++) {
+            FoodItem item = orderItems.get(i);
+            System.out.println((i+1) +  " : " + item.getFoodName());;
+        }
     };
-
-    public void setStaffName(String staffName) {
-        this.staffName = staffName;
-    };
-
-
-    // added this part
-    public String getStaffGender() {
-        return this.staffGender;
-    };
-
-    public void setStaffGender(String staffGender) {
-        this.staffGender = staffGender;
-    };
-
-    public String getStaffID() {
-        return this.staffID;
-    };
-
-    public void setStaffID(String staffID) {
-        this.staffID = staffID;
-    };
-
-
-    public void getJobTitle(String jobTitle)
-    {
-        return this.jobTitle;
-    };
-
-    public void setJobTitle(String jobTitle)
-    {
-        this.jobTitle = jobTitle;
-    };
-    // to here
 
     public int getTableNo() {
         return this.tableNo;
@@ -93,43 +91,93 @@ public class OrderInvoice {
         this.tableNo = tableNo;
     };
 
-    public String getTimeStamp() {
-        return this.timeStamp;
-    };
-
-    public void setTimeStamp(String timeStamp) {
-        this.timeStamp = timeStamp;
-    };
-
-    public String getDateStamp() {
-        return this.dateStamp;
-    };
-
-    public void setDateStamp(String dateStamp) {
-        this.dateStamp = dateStamp;
-    };
-
-    public double getSalesTax() {
-        return this.salesTax;
-    };
-
-    public void setSalesTax(double salesTax) {
-        this.salesTax = salesTax;
-    };
-
-    public double getServiceCharge() {
-        return this.serviceCharge;
-    };
-
-    public void setServiceCharge(double serviceCharge) {
-        this.serviceCharge = serviceCharge;
-    };
-
     public double getTotalSum() {
         // Add all the values
-        totalSum = 0.0;
-        for(int i = 0; i < this.orderItems.size(); i++){
-            totalSum += orderItems.get(i).get??price();
+        for (int i = 0; i < this.orderItems.size(); i++) {
+            FoodItem item = orderItems.get(i);
+            this.totalSum += item.getprice();
+        }
         return this.totalSum;
     }
+
+    public String currentTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public void printInvoice(){
+        // list the order details (eg, table number, timestamp)
+        // a complete breakdown of order items details with taxes details.
+        System.out.println("############ Invoice ###########");
+        System.out.println("Time      : " + currentTime());
+        System.out.println("Table No  : " + getTableNo());
+        System.out.println("Served By : " + getStaffName());
+        System.out.println("################################");
+        for (int i = 0; i < this.orderItems.size(); i++) {
+            FoodItem item = orderItems.get(i);
+            System.out.println(item.getFoodName() + " : " + item.getFoodPrice());
+        }
+        System.out.println("SubTotal  : " + getTotalSum());
+        double tax = (salesTax + serviceCharge) * getTotalSum();
+        System.out.println("Tax       : " + tax);
+        System.out.println("Total     : " + getTotalSum() + tax);
+        System.out.println("###### End of Transaction ######");
+        System.out.println("################################");
+    }
+    // public double getSalesTax() {
+    //     return this.salesTax;
+    // };
+
+    // public void setSalesTax(double salesTax) {
+    //     this.salesTax = salesTax;
+    // };
+
+    // public double getServiceCharge() {
+    //     return this.serviceCharge;
+    // };
+
+    // public void setServiceCharge(double serviceCharge) {
+    //     this.serviceCharge = serviceCharge;
+    // };
+
+    // public void setTotalSum(double totalSum) {
+    //     this.totalSum = totalSum;
+    // }
+
+    // Ambiguous, don't think we should do this 
+    // This should be initialize in constructo using staff object
+
+    // public String getStaffName() {
+    //     return staffName;
+    // }
+
+    // public void setStaffName(String staffName) {
+    //     this.staffName = staffName;
+    // }
+
+    // public String getStaffGender() {
+    //     return staffGender;
+    // }
+
+    // public void setStaffGender(String staffGender) {
+    //     this.staffGender = staffGender;
+    // }
+
+    // public String getJobTitle() {
+    //     return jobTitle;
+    // }
+
+    // public void setJobTitle(String jobTitle) {
+    //     this.jobTitle = jobTitle;
+    // }
+
+    // public int getStaffID() {
+    //     return staffID;
+    // }
+
+    // public void setStaffID(int staffID) {
+    //     this.staffID = staffID;
+    // }
+
 }
