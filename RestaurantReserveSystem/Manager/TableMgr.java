0e700eaf0105a0f5317948415f2e783ae293a3e7
Jovian20 <92246107+Jovian20@users.noreply.github.com>
2021-10-30 22:03:51 +0800

    Update TableMgr.java

--- RestaurantReserveSystem/Manager/TableMgr.java
+++ RestaurantReserveSystem/Manager/TableMgr.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TableMgr {
+   
+    private static ArrayList<Table> tables = new ArrayList<Table>();
+
+    public static void addRestTables(int num){
+        for(int i = 0; i < num; i++){
+            int nseats;
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter number of seats: ");
+            nseats = sc.nextInt();
+            tables.add(new Table(i, nseats));
+        }
+    }
+
+    public static void showTableList(){
+        System.out.println(tables);
+    }
+
+    public static void deleteRestTables(int index){
+        tables.remove(index-1);
+        System.out.println("Table %f has been removed.");
+    }
+}
 
