cbd07de4d95c31b9bbf949865a4fd5e10f9b02a3
teamoteh <lim1473@gmail.com>
2021-11-01 02:39:59 +0800

    Updated the Food Types to match extension FoodItem

--- RestaurantReserveSystem/MenuClass/Dessert.java
+++ RestaurantReserveSystem/MenuClass/Dessert.java
@@ -1,42 +1,15 @@
 package MenuClass;
 
-public class Dessert {
-    private String dessertName;
-    private double dessertPrice;
-    private String dessertDesc;
+import Entities.FoodItem;
+
+public class Dessert extends FoodItem {
 
     public Dessert() {
-        dessertName = null;
-        dessertPrice = 0.0;
-        dessertDesc = null;
+        super();
     }
 
-    public Dessert(String name, double price) {
-        this.dessertName = name;
-        this.dessertPrice = price;
+    public Dessert(String name, double price, String description) {
+        super(name, price, description);
     }
 
-    public String getDessertName() {
-        return this.dessertName;
-    }
-
-    public double getDessertPrice() {
-        return this.dessertPrice;
-    }
-
-    public String getDessertDesc() {
-        return this.dessertDesc;
-    }
-
-    public void setDessertName(String name) {
-        this.dessertName = name;
-    }
-
-    public void setDessertPrice(double price) {
-        this.dessertPrice = price;
-    }
-
-    public void setDessertDesc(String desc) {
-        this.dessertDesc = desc;
-    }
 }
--- RestaurantReserveSystem/MenuClass/Drink.java
+++ RestaurantReserveSystem/MenuClass/Drink.java
@@ -1,42 +1,15 @@
 package MenuClass;
 
-public class Drink {
-    private String drinkName;
-    private double drinkPrice;
-    private String drinkDesc;
+import Entities.FoodItem;
+
+public class Drink extends FoodItem{
 
     public Drink() {
-        drinkName = null;
-        drinkPrice = 0.0;
-        drinkDesc = null;
+        super();
     }
 
-    public Drink(String name, double price) {
-        this.drinkName = name;
-        this.drinkPrice = price;
+    public Drink(String name, double price, String description) {
+        super(name, price, description);
     }
 
-    public String getDrinkName() {
-        return this.drinkName;
-    }
-
-    public double getDrinkPrice() {
-        return this.drinkPrice;
-    }
-
-    public String getDrinkDesc() {
-        return this.drinkDesc;
-    }
-
-    public void setDrinkName(String name) {
-        this.drinkName = name;
-    }
-
-    public void setDrinkPrice(double price) {
-        this.drinkPrice = price;
-    }
-
-    public void setDrinkDesc(String desc) {
-        this.drinkDesc = desc;
-    }
 }
