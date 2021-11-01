e2e02ad511417c7067684927f633f295df66250a
teamoteh <lim1473@gmail.com>
2021-11-02 03:51:07 +0800

    changed get to Read function

--- RestaurantReserveSystem/Manager/TableMgr.java
+++ RestaurantReserveSystem/Manager/TableMgr.java
@@ -1,5 +1,7 @@
 package Manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import Entities.Table;
@@ -7,8 +9,26 @@ import db.Restaurant;
 
 public class TableMgr {
    
-    private static ArrayList<Table> tables = Restaurant.tables;
+    private static ArrayList<Table> tables = new ArrayList<Table>();
 
+    //FileReader for Tables 
+    public void readTables(ArrayList<Table> tables) throws FileNotFoundException
+	      {
+      		  File file = new File("//datatxt/Table.txt");
+      		  Scanner sc = new Scanner(file);
+		      sc.useDelimiter("\\s*,\\s*");
+	
+		      do{
+		    	  	int tableID = Integer.parseInt(sc.next());
+		          	int tableMaxSeats = Integer.parseInt(sc.next());
+		            
+		            Table table = new Table(tableID, tableMaxSeats);
+		            tables.add(table);
+		      }
+		      while(sc.hasNextLine());     
+		     sc.close();
+	      }
+    //not sure if need addRestTables 
     public static void addRestTables(int num){
         for(int i = 0; i < num; i++){
             int nseats;
