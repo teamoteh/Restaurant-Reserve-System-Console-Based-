12943f5478bf0d3415bf5ef331e8c7a23bce2177
teamoteh <lim1473@gmail.com>
2021-11-02 03:42:19 +0800

    done up staff mgr to store staff arraylist

--- RestaurantReserveSystem/Manager/StaffMgr.java
+++ RestaurantReserveSystem/Manager/StaffMgr.java
@@ -1,14 +1,18 @@
 package Manager;
 
 import db.Restaurant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import Entities.Staff;
 public class StaffMgr{
-    private static ArrayList<Staff> staffList = Restaurant.staffs;
+    public static ArrayList<Staff> staffList = new ArrayList<Staff>();
 
     public void getStaffIdentity(int id ){
         for (Staff staff : this.staffList){
-            staff = new Staff();
             if(staff.getStaffId() == id){
                 System.out.println("The Staff's Name is:" + staff.getStaffName());
                 System.out.println("The Staff's Job Title is:" + staff.getJobTitle());
@@ -17,4 +21,22 @@ public class StaffMgr{
             }
         }
     }
+
+    public void getStaffs(ArrayList<Staff> staffs) throws FileNotFoundException
+	      {
+      		  File file = new File("/Users/pranwanth/eclipse-workspace/assignmentTest/src/assignmentTest/drinks.txt");
+      		  Scanner sc = new Scanner(file);
+		      sc.useDelimiter("\\s*,\\s*");
+	
+		      do{
+		    	  	String staffName = sc.next();
+		          	String staffGender = sc.next();
+		          	int staffID = Integer.parseInt(sc.next());
+		            String staffTitle = sc.next();
+		            Staff staffPerson = new Staff(staffName, staffGender, staffID, staffTitle);
+		            staffs.add(staffPerson);
+		      }
+		      while(sc.hasNextLine());     
+		     sc.close();
+	      }
 }
