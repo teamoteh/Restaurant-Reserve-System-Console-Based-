30295b7c399dcf9b2544ffe6fb448db638d19d64
Shannen <shannenlee21@yahoo.com>
2021-11-04 11:16:27 +0800

    Update ReservationMgr.java

--- RestaurantReserveSystem/Manager/ReservationMgr.java
+++ RestaurantReserveSystem/Manager/ReservationMgr.java
@@ -1,18 +1,47 @@
 package Manager;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 //import datatxt.FileReaderWriter;
 import Entities.Reservation;
+import datatxt.FileReaderWriter;
+
 //import Entities.Table;
+import java.io.*;
+import java.util.Scanner;
 
 public class ReservationMgr {
 
      protected static ArrayList<Reservation> r;
     
-    public ReservationMgr(){
- 
+    public ReservationMgr() /*throws FileNotFoundException*/ {
+        
         r = new ArrayList<Reservation>();
+        /*File file = new File("./Reservation.txt");
+        Scanner sc = new Scanner(file);
+        int i=0;
+        sc.useDelimiter("\\s*,\\s*");
+    
+    while(sc.hasNextLine());     
+        {
+            r.get(i).setNumOfPax(sc.nextInt());
+            r.get(i).setCustName(sc.next());
+            r.get(i).setCustContact(sc.nextInt());
+            r.get(i).setReserveTime(LocalTime.parse(sc.nextLine()));
+            r.get(i).setReserveDate(LocalDate.parse(sc.nextLine()));
+            i++;
+        }
+
+        sc.close();
+        FileReaderWriter fi = new FileReaderWriter();
+	        try {
+				fi.getReservation(r);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
     }
+    
 
     public static void checkReservationExpiry() {
 
@@ -62,6 +91,14 @@ public class ReservationMgr {
 
     public static void displayReservation ()
     {
+
+        FileReaderWriter fi = new FileReaderWriter();
+        try {
+            fi.getReservation(r);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         System.out.println("Date    Time      Name     Pax      Contact      TableNo");
         for (int i = 0; i < r.size(); i++) {
             System.out.println(r.get(i).getReserveDate() + "    " + r.get(i).getReserveTime()+ "    " + r.get(i).getCustName() + "    " + r.get(i).getNumOfPax()+ "    " + r.get(i).getCustContact()+ "    " + r.get(i).getTableNo());
